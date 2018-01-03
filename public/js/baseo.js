d3b1fcfb52f0a2f01e6bf540c6bae9bb7a5d7e39
youxianyen <youxianyen@sina.com>
2018-01-03 09:24:12 +0800

    update index.blade.php

---
+++ public/js/baseo.js
@@ -0,0 +1,191 @@
+;(function(){
+	'use strict';
+	
+	angular.module('xiaohu', [
+		'ui.router',
+		])
+	.config(function($interpolateProvider, 
+		              $stateProvider, 
+		              $urlRouterProvider)
+	{
+        $interpolateProvider.startSymbol('[:');
+        $interpolateProvider.endSymbol(':]');
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+          .state('home', {
+          	url: '/home',
+          	templateUrl: "home.tpl"
+          })
+          .state('signup', {
+          	url: '/signup',
+          	templateUrl: "signup.tpl"
+          })
+          .state('login', {
+          	url: '/login',
+          	templateUrl: "login.tpl"
+          })
+          .state('question', {
+            abstract: true,
+            url: '/question',
+            template: '<div ui-view></div>'
+          })
+          .state('question.add', {
+            url: '/add',
+            templateUrl: 'question.add.tpl'
+          })
+	})
+	
+  .service('UserService', [
+    '$state',
+    '$http',
+    function($state, $http){
+      var me = this;
+      me.signup_data = {};
+      me.login_data = {};
+      me.new_question = {};
+      me.signup = function()
+      {
+        $http.post('/api/signup', me.signup_data)
+        .then(function(r){
+          if (r.data.status)
+          {
+            me.signup_data = {};            
+            $state.go('login');
+          }
+        }, function(e){
+          console.log(e);
+        })
+      }
+
+      me.login = function ()
+      {
+        $http.post('/api/login', me.login_data)
+          .then(function(r){
+            if (r.data.status) 
+            {
+              /*$state.go('home');*/
+              location.href = '/';
+            }
+            else 
+            {
+              me.login_failed = true;
+            }
+          }, function(){
+            
+          })
+      }
+
+      me.username_exists = function()
+      {
+        $http.post('api/user/exist', 
+          {username: me.signup_data.username})
+        .then(function(r){
+          if (r.data.status && r.data.data.count)
+            {me.signup_username_exists = true;}
+          else
+            {me.signup_username_exists = false;}
+          }, function(e){
+          console.log('e', e); 
+          })
+      }
+    }])
+
+  .controller('SignupController', [
+        '$scope', 
+        'UserService',
+        function($scope, UserService)
+        {
+            $scope.User = UserService;
+
+            $scope.$watch(function(){
+              return UserService.signup_data;
+            }, function(n, o){
+              
+              if (n.username != o.username) //监控修改了username
+              {
+                UserService.username_exists(); //执行
+              }
+            }, true);
+
+        }])
+
+  .controller('LoginController', [
+    '$scope', 
+    'UserService',
+    function($scope, UserService){
+      $scope.User = UserService;
+  }])
+
+  .service('QuestionService', [
+    '$http', 
+    '$state',
+    function($http, $state)
+    {
+        var me = this;
+        me.new_question = {};
+
+        me.go_add_question = function()
+        {
+          $state.go('question.add');
+        }     
+
+        me.add = function()
+        {
+          if (!me.new_question.title)
+            return;
+
+          $http.post('/api/question/add', me.new_question)
+            .then(function(r){
+              if (r.data.status)
+              {
+                me.new_question = {};
+                $state.go('home');
+              }
+            }, function(e){
+
+            })
+        }
+    }
+    ])
+
+  .controller('QuestionAddController', [
+    '$scope',
+    'QuestionService',
+    function($scope, QuestionService){
+      $scope.Question = QuestionService;
+  }])
+
+  .service('TimelineService', [
+    '$http', 
+    function($http)
+    {
+      var me = this;
+      me.data = [];
+      me.get = function(conf)
+      {
+        $http.post('api/timeline', conf)
+          .then(function (){
+            if (r.data.status)
+            {
+              me.data = me.data.concat(r.data.data);
+            }
+            else 
+            {
+              console.log('network error');
+            }
+          }, function (){
+            console.log('network error');
+          })
+      }
+    }])
+
+/*  .controller('HomeController', [
+    '$scope',
+    'TimelineService'
+    function($scope, TimelineService){
+    $scope.timeline = TimelineService;
+    //TimelineService.get();
+    }])*/
+})();
---
+++ resources/views/indexo.blade.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="zh" ng-app="xiaohu">
+<head>
+	<meta charset="utf-8">
+	<title>晓乎</title>
+	<link rel="stylesheet" type="text/css" href="/node_modules/normalize-css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="/css/base.css">
+	<script type="text/javascript" src="/node_modules/jquery/dist/jquery.js"></script>
+	<script type="text/javascript" src="/node_modules/angular/angular.js"></script>
+	<script type="text/javascript" src="/node_modules/angular-ui-router/release/angular-ui-router.js"></script>
+	<script type="text/javascript" src="/js/base.js"></script>
+</head>
+<body>
+<div class="navbar clearfix">
+<div class="container">
+	<div class="fl">
+		<div class="navbar-item brand">晓乎</div>
+			<form ng-submit="Question.go_add_question()" id="quick_ask" ng-controller="QuestionAddController">
+				<div class="navbar-item">
+			        <input ng-model="Question.new_question.title" type="text" name="">			        			
+				</div>
+                <div class="navbar-item">
+			        <button type="submit">提问</button>			        			
+				</div>
+			</form>
+	</div>
+	<div class="fr">
+		<a ui-sref="home" class="navbar-item">首页</a>
+		@if(is_logged_in())
+		<a ui-sref="{{url('/logout')}}" class="navbar-item">{{session('username')}}</a>
+		<a href="{{url('api/logout')}}" class="navbar-item">登出</a>
+		@else
+		<a ui-sref="login" class="navbar-item">登录</a>
+		<a ui-sref="signup" class="navbar-item">注册</a>
+		@endif
+	</div>
+</div>
+</div>
+
+<div class="page">
+	<div ui-view></div>
+</div>
+
+</body>
+<!-- .page .home -->
+<script type="text/ng-template" id="home.tpl">
+<div ng-controller="HomeController" class="home card container">	
+	<h1>最近动态</h1>
+	<div class="hr"></div>
+	<div class="item-set">
+		<div ng-if="" ng-repeat="item in Timeline.data" class="item">
+			<div class="vote"></div>
+			<div class="feed-item-content">
+				<div ng-if="item.question_id" class="content-act">[: item.user.username :]添加了回答</div>
+				<div ng-if="!item.question_id" class="content-act">[: item.user.username :]添加了提问</div>
+				<div class="title">[: item.title :]</div>
+				<div class="content-owner">[: item.user.username :]</div>
+				<div class="content-main">
+					[: item.title :]
+				</div>
+				<div class="action-set">
+					<div class="comment">评论</div>
+				</div>
+				<div class="comment-block">
+					<div class="hr"></div>
+                    <div class="comment-item-set">
+                    	<div class="rect"></div>
+                    	<div class="comment-item clearfix">
+                            <div class="user">李明</div>
+                            <div class="comment-content">But researchers say those percentages of young children who believe in jolly old Saint Nick seem to have remained steady over the years.
+                            </div>
+                    	</div>
+                    	<div class="comment-item clearfix">
+                            <div class="user">李明</div>
+                            <div class="comment-content">But researchers say those percentages of young children who believe in jolly old Saint Nick seem to have remained steady over the years.
+                            </div>
+                    	</div>
+                    	<div class="comment-item clearfix">
+                            <div class="user">李明</div>
+                            <div class="comment-content">But researchers say those percentages of young children who believe in jolly old Saint Nick seem to have remained steady over the years.
+                            </div>
+                    	</div>
+                    </div>
+				</div>
+			</div>			
+		</div>
+		<div class="hr"></div>
+
+
+	</div>
+</div>	
+
+</script>
+
+<script type="text/ng-template" id="signup.tpl">
+<div ng-controller="SignupController" class="signup container">
+	<div class="card">
+		<h1>注册</h1>
+		<!-- [: User.signup_data :] -->
+		<form name="signup_form" ng-submit="User.signup()">
+			<div class="input-group">
+				<label>用户名</label>
+			    <input type="text" 
+			    ng-minlength="4" 
+			    ng-maxlength="24" 
+			    name="username" 
+			    ng-model="User.signup_data.username" 
+			    ng-model-options="{debounce: 500}"
+			    required>
+			    <div ng-if="signup_form.username.$touched" class="input-error-set">
+			    	<div ng-if="signup_form.username.$error.required">
+			    	用户名为必填项
+			        </div>
+			        <div ng-if="signup_form.username.$error.maxlength || signup_form.username.$error.minlength">
+			        	用户名长度需要在4-24位之间
+			        </div>
+			        <div ng-if="User.signup_username_exists">
+			        	用户名已存在
+			        </div>
+			    </div>
+			</div>
+			<div class="input-group">
+				<label>密码</label>
+			    <input name="password" 
+			    type="password" 
+			    ng-minlength="6" 
+			    ng-maxlength="255" 
+			    ng-model="User.signup_data.password" 
+			    required>
+			    <div ng-if="signup_form.password.$touched" class="input-error-set">
+			    	<div ng-if="signup_form.password.$error.required">
+			    	密码为必填项
+			        </div>
+			        <div ng-if="signup_form.password.$error.maxlength || signup_form.password.$error.minlength">
+			        	用户名长度需要在6-255位之间
+			        </div>
+			    </div>
+			</div>
+			<button class="primary" type="submit" ng-disabled="signup_form.$invalid">注册</button>
+		</form>
+	</div>
+</div>
+</script>
+
+<script type="text/ng-template" id="login.tpl">
+<div ng-controller="LoginController" class="login container">
+    <div class="card">
+    	<h1>登录</h1>
+    	<form name="login_form" ng-submit="User.login()">    
+    	<div class="input-group">
+    		<label>用户名</label>
+    		<input type="text" 
+    		ng-model="User.login_data.username" 
+    		name="username" 
+    		required>
+    	</div>	
+    	<div class="input-group">
+    		<label>密码</label>
+    		<input type="password"
+    		 ng-model="User.login_data.password" 
+    		 name="password" 
+    		 required>
+    	</div>		
+    	<div ng-if="User.login_failed" class="input-error-set">
+    		用户名和密码有误
+    	</div>
+    	<div class="input-group">
+    		<button type="submit" 
+    		        class="primary"
+    		        ng-disabled="login_form.username.$error.required || 
+    		        login_form.password.$error.required">登录</button>
+    	</div>		
+    	</form>
+    </div>
+</div>
+</script>
+
+<script type="text/ng-template" id="question.add.tpl">
+<div ng-controller="QuestionAddController" class="question-add container">
+	<div class="card">
+		<form name="question_add_form" ng-submit="Question.add()">
+			<div class="input-group">
+				<label>问题标题</label>
+				<input type="text"
+				 ng-minlength="5"
+				 ng-maxlength="255"
+				 ng-model="Question.new_question.title" 
+				 name="title" 
+				 required
+				>
+			</div>
+			<div class="input-group">
+				<label>问题描述</label>
+				<textarea type="textarea" 
+				name="desc" 
+				ng-model="Question.new_question.desc" 
+				>
+				</textarea>
+			</div>
+			<div class="input-group">
+				<button type="submit" 
+    		        class="primary"
+    		        ng-disabled="question_add_form.$invalid"
+    		    >提交</button>				
+			</div>
+		</form>
+	</div>
+</div>
+</script>
+</html>
